43403d2cbf7f197802d2f49ce7ff3718e858f76c
xiaoruanzhu <xiaoruanzhu@users.noreply.github.com>
2018-08-24 17:53:16 +0800

    数据描述

---
+++ dataDescription.lua
@@ -0,0 +1,71 @@
+
+
+function fwrite( fmt,... )
+	return io.write(string.format(fmt, ...))
+end
+
+function writeheader()
+	io.write([[
+		<html>
+		<head><title>Projects using lua</title><head>
+		<body bgcolor="#FFFFFF">
+		网页body1<a href="home.html">Lua</a>
+		<br>
+		]])
+end
+function entry1(o)
+	count =count+1
+	local title = o.title or '(no title)'
+	fwrite('<li><a href="#%d">%s</a>\n' , count,title)
+end
+
+function entry2(o)
+	count= count+1
+	fwrite('<hr>\n<h3>\n')
+
+	local href =o.url and string.format('href="%s"', o.url) or ''
+	local title =o.title or o.org or 'org'
+	fwrite('<a name="%d"%s>%s</a>\n' , count,href,title)
+	if o.title and o.org then
+		fwrite('<br>\n<small><em>%s</em></small>',o.org)
+	end
+	fwrite('\n</h3>\n')
+
+	if o.description then
+		fwrite('%s<p>\n',string.gsub(o.description,'\n\n+' ,'<p>\n'))
+	end
+
+	if o.email then
+		fwrite('Contact:<a href="mailto :%s">%s</a>\n', o.email,o.contact or o.email)
+	elseif o.contact then
+			fwrite('Contact:%s\n', o.contact)
+	end
+end
+
+
+function writetail()
+	fwrite('</body></html>\n')
+end
+
+
+data ={
+	title ="Tecgraf",
+	org="Computer Graphics Technology Group,PUC-Rio",
+	url ="http://www.tecgraf.puc-rio.br/",
+}
+
+
+writeheader()
+count = 0
+entry = entry1
+fwrite('<ul>\n')
+entry(data)
+fwrite('</ul>\n')
+
+count = 0
+entry = entry2
+entry(data)
+
+writetail()
+
+
