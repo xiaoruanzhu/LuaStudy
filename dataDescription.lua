d91037bd4794e63237048faca5c1c67fcbab8953
xiaoruanzhu <41225835+xiaoruanzhu@users.noreply.github.com>
2018-08-26 16:01:20 +0800

    数据描述，马尔科夫链

--- dataDescription.lua
+++ dataDescription.lua
@@ -68,4 +68,58 @@ entry(data)
 
 writetail()
 
+function prefix(w1,w2)
+	return w1 .." " ..w2
+end
 
+function allwords()
+	local line=io.read()
+	local pos = 1
+	return function ()
+		while line do
+			local s,e = string.find(line,"%w+",pos)
+			if s then
+				pos =e+1
+				return string.sub(line,s,e)
+			else
+				line =io.read()
+				pos=1
+			end
+		end
+		return nil
+	end
+end
+
+local statetab={}
+function insert(index,value)
+	local list = statetab[index]
+	if list==nil then
+		statetab[index] ={value}
+	else
+		list[#list +1]=value
+	end
+end
+
+local N=2
+local MAXGET = 5
+local NOWORD = "\n"
+
+--构建table
+local w1,w2 = NOWORD,NOWORD
+for w in allwords() do
+	insert(prefix(w1,w2),w)
+	w1=w2;w2=w
+end
+insert(prefix(w1,w2),NOWORD)
+
+--生成文本
+w1=NOWORD;w2=NOWORD
+for i=1,MAXGET do
+	local list = statetab[prefix(w1,w2)]
+	--从列表中选择一个随机项
+	local r = math.random(#list)
+	local nextword = list[r]
+	if nextword ==NOWORD then return end
+	io.write(nextword," ")
+	w1=w2;w2=nextword
+end
