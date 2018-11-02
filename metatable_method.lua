115b14df3ee47116e630a22e38cb3073a86d329c
xiaoruanzhu <xiaoruanzhu@users.noreply.github.com>
2018-11-02 13:08:39 +0800

    方法  合并冲突

--- metatable_method.lua
+++ metatable_method.lua
@@ -20,12 +20,45 @@ function Set.union(a,b)
 end
 --求交集
 function Set.intersection(a,b)
+
+t={}
+print(getmetatable(t))
+t1={}
+setmetatable(t,t1)
+--assert(getmetatable(t)==t1)
+print(getmetatable(t))
+print(getmetatable("4"))
+print(getmetatable("b"))
+
+Set={}
+function Set.new(l)
+	local set = {}
+	for _,k in pairs(l) do
+		set[k] =true
+	end
+	return set
+end
+
+function Set.union(a,b)
+	local res = Set.new{}
+	for k,v in pairs(a) do
+		res[k]=true
+	end
+	for k,v in pairs(b) do
+		res[k]=true
+	end
+	return res
+end
+
+function Set.intersection(a,b )
+
 	local res = Set.new{}
 	for k in pairs(a) do
 		res[k]=b[k]
 	end
 	return res
 end
+
 --转string
 function Set.tostring(set)
 	local l = {} --用于存放集合中所有元素的集合
@@ -35,10 +68,19 @@ function Set.tostring(set)
 	return "{" ..table.concat( l, ", ") .."}"
 end
 
+
+function Set.tostring(set)
+	local l = {}
+	for k,v in pairs(set) do
+		l[#l+1]=k
+		--print(k) print(v)
+	end
+	return "{" ..table.concat(l, ", ") .."}"
+end
+
 function Set.print(s)
 	print(Set.tostring(s))
 end
-
 
 
 local mt ={}   --存放元方法
@@ -54,9 +96,21 @@ function Set.new(l)  --第2版
 end
 --从此以后，所有由Set.new创建的集合都具有一个相同的元素
 s1 =Set.new{10,20,30,50}
+
+local mt={}
+function Set.new(l)
+	local set ={}
+	setmetatable(set,mt)
+	for _,v in ipairs(l) do set[v] =true end
+	return set
+end
+
+s1=Set.new{10,20,30,40}
+
 s2 =Set.new{30,1}
 print(getmetatable(s1))
 print(getmetatable(s2))
+
 
 --添加元方法
 mt.__add=Set.union
@@ -154,4 +208,11 @@ end
 
 local m = require "io"
 m.write("hello world\n")
+
+mt.__add=Set.union
+s3 =s1+s2
+Set.print(s3)
+mt.__mul =Set.intersection
+Set.print((s1+s2)*s1)
+
 
